2638c3605b047f378d6037db7701fc4585360d56
tiff3b <tiffany3berger@gmail.com>
2025-11-22 11:35:47 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// chrome://extensions/
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+const getLeads = JSON.parse(localStorage.getItem("myLeads"))
+
+if (getLeads) {
+    myLeads = getLeads
+    render(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/tiff-berger/"}
+]
+
+tabBtn.addEventListener("click", function (){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+
+deleteBtn.addEventListener('dblclick', function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    render(myLeads)
+})
+
+
